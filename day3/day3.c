c0750b21144c30b490a94e9b4e7888f0d6c138c0
Tim Hvaal <timothy.hvaal@bluefruit.co.uk>
2022-12-06 10:20:19 +0000

    Day 3. star 2 complete

--- day3/day3.c
+++ day3/day3.c
@@ -13,6 +13,13 @@
 //                 total += item
 //                 break out of line loops
 
+// star 2
+// store three lines
+// check each letter one by one against second line
+// if letter in second check third
+// if item in third that's the badge
+// prioritise the item and add to total
+
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -28,13 +35,13 @@ int main(void)
     FILE* file = fopen("day3/input.txt", "r");
     perror("open text file");
 
-    char line[100];
+    char line[50], line2[50], line3[50];
     size_t line_len = 0, halfway = 0, search_pos = 0, compare_pos = 0, item_total = 0;
     char item;
 
     while(!feof(file))
     {
-        fgets(line, 100, file);
+        fgets(line, 50, file);
 
         while(line[line_len] != '\n')
             line_len++;
@@ -59,8 +66,49 @@ int main(void)
         line_len = 0;
         halfway = 0;
     }
-    printf("item_total: %d", item_total);
+
+    printf("item_total: %d\n", item_total);
+    item_total = 0;
+
+    // star 2
+    fseek(file, 0, SEEK_SET);
+    while(!feof(file))
+    {
+        item = '\0';
+        fgets(line, 50, file);
+        fgets(line2, 50, file);
+        fgets(line3, 50, file);
+
+        for (int i = 0; line[i] != '\n'; i++)
+        {
+            for (int j = 0; line2[j] != '\n'; j++)
+            {
+                if(line[i] == line2[j])
+                {
+                    for(int k = 0; line3[k] != '\n'; k++)
+                    {
+                        if(line[i] == line3[k])
+                        {
+                            item = line[i];
+                            item_total += prioritise(item);
+                            item = '!';
+                            break;
+                        }
+                    }
+
+                    if(item == '!')
+                        break;
+                }
+            }
+
+            if (item == '!')
+                break;
+        }
+    }
+    printf("star2_total: %d\n", item_total);
 }
+
+
 
 int prioritise(char item)
 {
